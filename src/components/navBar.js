458934ab33d3de92a69fae75a97f50058749d37e
LisandroCausa <lisandro.anexa@gmail.com>
2022-12-05 00:55:17 -0300

    Add tabbed browsing

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,11 +1,41 @@
+import mainTitle from "./mainTitle";
+
 export default function() {
 	const nav = document.createElement("nav");
 	nav.id = "navbar";
 	const list = document.createElement("ul");
 
 	const home = navOption("Home");
+	home.classList.add("selected");
 	const menu = navOption("Menu");
 	const contact = navOption("Contact");
+
+	home.addEventListener("click", () => {
+		if(home.classList.contains("selected") == false)
+		{
+			const contentDiv = document.getElementById("content");
+			emptyDOMElement(contentDiv);
+			contentDiv.appendChild(mainTitle());
+		}
+	});
+
+	menu.addEventListener("click", () => {
+		if(menu.classList.contains("selected") == false)
+		{
+			const contentDiv = document.getElementById("content");
+			emptyDOMElement(contentDiv);
+			contentDiv.appendChild(document.createElement("h1"));
+		}
+	});
+
+	contact.addEventListener("click", () => {
+		if(contact.classList.contains("selected") == false)
+		{
+			const contentDiv = document.getElementById("content");
+			emptyDOMElement(contentDiv);
+			contentDiv.appendChild(document.createElement("p"));
+		}
+	});
 
 	list.appendChild(home);
 	list.appendChild(menu);
@@ -14,8 +44,11 @@ export default function() {
 	const options = list.childNodes;
 	options.forEach(element => {
 		element.addEventListener("click", () => {
-			removeClassFromChilds(list, "selected");
-			element.classList.add("selected");
+			if(element.classList.contains("selected") == false)
+			{
+				removeClassFromChilds(list, "selected");
+				element.classList.add("selected");
+			}
 		});
 	});
 
@@ -33,4 +66,10 @@ function navOption(text) {
 	const li = document.createElement("li");
 	li.textContent = text;
 	return li;
+}
+
+function emptyDOMElement(element) {
+	while (element.firstChild) {
+		element.removeChild(element.lastChild);
+	}
 }
