3f99df522b45d4b1431c92ee7a91a8e82bb28629
LisandroCausa <lisandro.anexa@gmail.com>
2022-12-08 23:47:07 -0300

    Improve main title

--- src/components/mainTitle.js
+++ src/components/mainTitle.js
@@ -1,6 +1,6 @@
 export default function() {
 	const h1 = document.createElement("H1");
-	h1.textContent = "The best grilled meat";
+	h1.textContent = "THE BEST GRILLED MEAT";
 	h1.id = "main-title";
 	return h1;
 }
